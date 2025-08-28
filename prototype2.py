75b22f85bbe424a9ac4afa85163b04962902c080
Prxcz <arthurvalinbentivoglio@gmail.com>
2025-08-28 15:17:46 +1000

    Update prototype2.py

--- prototype2.py
+++ prototype2.py
@@ -1,15 +1,15 @@
 # VERSION 2 
 energy_sources_v2 = [
-"solar_pv", "wind", "battery", "grid", "biomass"
+  "solar_pv", "wind", "battery", "grid", "biomass"
 ]
 
 sensors_v2 = ["temp", "flow", "power", "weather", "price_feed"]
 
 ai_tasks_v2 = {
-"demand_forecast": "guess heat need next 24h",
-"solar_forecast": "predict sun",
-"maintenance": "spot problems",
-"control": "when to charge/discharge"
+  "demand_forecast": "guess heat need next 24h",
+  "solar_forecast": "predict sun",
+  "maintenance": "spot problems",
+  "control": "when to charge/discharge"
 }
 
 steps_v2 = ["collect_data()", "train_forecasts()", "optimise_charging()"]
