e5e7a3247cf9c283b4c07dc17e57d039abcd68d6
Prxcz <arthurvalinbentivoglio@gmail.com>
2025-08-28 15:17:21 +1000

    Update prototype1.py

--- prototype1.py
+++ prototype1.py
@@ -4,8 +4,8 @@ energy_sources = ["solar_pv", "wind", "battery"]
 sensors = ["temp", "flow", "power"]
 
 ai_tasks = {
-"demand_forecast": "guess heat need",
-"control": "charge/discharge"
+  "demand_forecast": "guess heat need",
+  "control": "charge/discharge"
 }
 
 print("v1: only basic TES plan")
