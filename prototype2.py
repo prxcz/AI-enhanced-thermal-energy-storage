e45d9a73753f5c88511e3b3c19bb3a399c4f14c8
Prxcz <arthurvalinbentivoglio@gmail.com>
2025-08-28 15:14:35 +1000

    Create prototype2.py
    adding functionals changes

---
+++ prototype2.py
@@ -0,0 +1,17 @@
+# VERSION 2 
+energy_sources_v2 = [
+"solar_pv", "wind", "battery", "grid", "biomass"
+]
+
+sensors_v2 = ["temp", "flow", "power", "weather", "price_feed"]
+
+ai_tasks_v2 = {
+"demand_forecast": "guess heat need next 24h",
+"solar_forecast": "predict sun",
+"maintenance": "spot problems",
+"control": "when to charge/discharge"
+}
+
+steps_v2 = ["collect_data()", "train_forecasts()", "optimise_charging()"]
+
+print("v2: more tasks but still not complete")
